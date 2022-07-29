96153869602ca6158de37edb7a1b978c307fcd62
Ian Harvey <108738675+Harvey1968@users.noreply.github.com>
2022-07-29 10:27:28 +0100

    Update PythonLab3_2_1_9.py

--- PythonLab3-2-1-9/PythonLab3-2-1-9/PythonLab3_2_1_9.py
+++ PythonLab3-2-1-9/PythonLab3-2-1-9/PythonLab3_2_1_9.py
@@ -1,10 +1,19 @@
 # 3.2.1.9 LAB: The break statement - Stuck in a loop
+#⠀⠀ ⠀⠀⣀⣤⣴⣶⣶⣦⣄⡀⠀⠀⠀⠀⠀ ⠀⢀⣤⣶⣶⣶⣦⣤⡀⠀⠀⠀⠀
+#⠀ ⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀ ⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀
+#⠀⠀⣾⣿⠟⠋⠉⠀ ⠀⠉⠙⠻⣿⣷⡀ ⣰⣿⣿⣿⠟⠉⠀ ⠀  ⠈⠙⣿⣷⠀⠀
+#⠀⢸⣿⠏⠀⠀⠀⠀⠀  ⠀  ⠀⠈⢻⣿⣿⣿⡿⠃⠀⠀⠀  ⠀  ⠀⠀⠸⣿⡇⠀
+#⠀⢸⣿⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⢀⣾⣿⣿⡿⠁⠀⠀⠀⠀⠀   ⠀⠀⠀⠀⣿⡇⠀
+#⠀⢸⣿⡆⠀⠀⠀⠀⠀  ⠀⠀⢀⣾⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀   ⠀⠀⠀⢰⣿⡇⠀
+#⠀⠀⢿⣿⣄⡀⠀⠀   ⢀⣴⣿⣿⣿⠟⠘⢿⣿⣦⣀⡀⠀⠀ ⢀⣀⣴⣿⡿⠀⠀
+#⠀⠀ ⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀ ⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀
+#⠀⠀ ⠀ ⠀⠈⠛⠻⠿⠿⠿⠛⠁⠀⠀⠀⠀ ⠀⠀⠈⠙⠻⠿⠿⠿⠛⠉⠀⠀⠀⠀;; art credit - www.emojicombos.com/infinity-ascii-art
 
 while True:
-    print('"Oops looks like you\'re trapped in my never ending loop!"') 
+    print('"Oops looks like you\'re trapped in my never ending loop! Ha ha!"') 
     secret = input("Enter the secret word to escape the loop: ")
     print()
-    # A 'string' is used as a conditional statement.
+    # The below condition entered as a 'string' is used as a conditional statement.
     if secret == "chupacabra":
         # If the secret word is entered then exit the loop.
         break
